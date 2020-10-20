2f4b19dc045e4c37f452c667215a56a08e89cd8f
Anne Wang <annewang@macbook-pro.attlocal.net>
2020-10-20 10:03:54 -0700

    uses Blueutil to run os commands, prints connected devices

---
+++ bluetooth_devices.py
@@ -0,0 +1,12 @@
+import subprocess
+import json
+
+output = subprocess.check_output("blueutil --format json --paired", shell=True)
+
+output_json = output.decode("utf-8").replace("'", '"')  # decodes the byte into a json
+
+print()
+data = json.loads(output_json)  # loads the json into a dictionary
+
+for ele in data:
+    print(ele["name"], " ==> Signal strength: ", ele["RSSI"])
