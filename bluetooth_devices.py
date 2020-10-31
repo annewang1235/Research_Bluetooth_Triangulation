a8f67ffbaf03cce983208d7f283ce4bc64e2aade
johnyu763 <johnyu763@gmail.com>
2020-10-31 12:19:08 -0700

    Pre-change

--- bluetooth_devices.py
+++ bluetooth_devices.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
         for ele in data:
             if("rawRSSI" in ele):
                 print(ele["name"] +" Signal Strength: "+str(ele["rawRSSI"]))
-        time.sleep(0.5)
+        time.sleep(0.1)
 
