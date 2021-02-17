f881afcd199eb8e3eb8dae5d1647faafae1a8bfe
Anne Wang <annewang@macbook-pro.attlocal.net>
2021-02-16 17:46:09 -0800

    fixed intersection coordinates, displays intersection outline

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ import os
 def collectDataSamples(bestFitLineDict, device_name, device_id):
     for j in range(20):
       # gets the RSSI data from specified device
-      samples, raw_samples = bluetooth_devices.getRSSISamples(50, device_id)
+      samples, raw_samples = bluetooth_devices.getRSSISamples(25, device_id)
 
       # gets the RSSI mode and distance between receiver & i's device
       rssi_median = {key:bluetooth_devices.getMedian(raw_sample) for (key, raw_sample) in raw_samples.items()}
       print("rssi_mode is " + str(rssi_median))
 
+      # gets the predicted distance between receiver and i's device
       for index in range(len(device_name)):
         predicted_distance = bestFitCalcs.getDistance(
             bestFitLineDict[device_name[index]][0],
@@ -63,27 +64,27 @@ def getRadiusOfBeacons(chosen_devices, chosen_device_names, bestFitLineDict):
     return radiusDict
 
 def withinBounds(bounds, actualCoord):
+    if (bounds == ()):
+        print("No intersection.")
+        return
+
     actual_x = actualCoord[0]
     actual_y = actualCoord[1]
 
     withinX = actual_x > bounds[0] and actual_x < bounds[2]
     withinY = actual_y > bounds[1] and actual_y < bounds[3]
 
-    printIsBetween(actual_x, bounds[0], bounds[2], withinX, "X")
-    printIsBetween(actual_y, bounds[1], bounds[3], withinY, "Y")
+    _printIsBetween(actual_x, bounds[0], bounds[2], withinX, "X")
+    _printIsBetween(actual_y, bounds[1], bounds[3], withinY, "Y")
 
 
 
-def printIsBetween(actualCoord, minBound, maxBound, within, dir):
+def _printIsBetween(actualCoord, minBound, maxBound, within, dir):
     if (within):
         print(str(actualCoord) + " is between " + str(minBound) + " and " + str(maxBound) + "; " + dir + "-dir")
     else:
         print(str(actualCoord) + " is not between " + str(minBound) + " and " + str(maxBound) + "; " + dir + "-dir")
 
-def marginOfError(predictedCoords, actualCoords):
-    dist_squared = (predictedCoords[0] - actualCoords[0])**2 + (predictedCoords[1] - actualCoords[1])**2
-    dist = math.sqrt(dist_squared)
-    return math.trunc(dist)
 
 if __name__ == "__main__":
     (
@@ -111,16 +112,10 @@ if __name__ == "__main__":
     print(device_positions)
     print(chosen_device_names)
 
-    bounds = draw_circle.get_intersection(
+    intersection = draw_circle.get_intersection(
         radiusDict, device_positions, chosen_device_names)
 
-    withinBounds(bounds, actualCoords)
-
-    midpointX = bestFitCalcs.midpoint(bounds[0], bounds[2])
-    midpointY = bestFitCalcs.midpoint(bounds[1], bounds[3])
-
-    marginOfError = marginOfError((midpointX, midpointY), actualCoords)
-    print(str(marginOfError) + " ft off from center of intersection.")
+    bestFitCalcs.getIntersectionCenter(intersection, actualCoords)
 
     draw_circle.visualize_circles(
-        radiusDict, device_positions, chosen_device_names, actualCoords)
+        radiusDict, device_positions, chosen_device_names, actualCoords, intersection)
